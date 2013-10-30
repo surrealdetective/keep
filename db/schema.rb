d5a5a64fbaf83c112b21c592a865ee2c9e0c5cf7
Leandra Tejedor <lmtejedor@gmail.com>
2013-10-29 22:16:44 -0400

    Readme updated with contribution information and live links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131013183843) do
+ActiveRecord::Schema.define(version: 20131013160157) do
 
   create_table "comments", force: true do |t|
     t.text     "comment"
